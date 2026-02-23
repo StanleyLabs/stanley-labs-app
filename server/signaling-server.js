d9e9cda546291d4ecf1a033bfd360105940d2439
StanleyLabs <28549205+KennyStanleyJr@users.noreply.github.com>
2026-02-23 18:42:22 -0500

    fix: enable CORS on signaling server for cross-origin Socket.IO

--- server/signaling-server.js
+++ server/signaling-server.js
@@ -1,7 +1,12 @@
 import { Server } from 'socket.io'
 
 export default function initSignaling(server) {
-    const io = new Server(server)
+    const io = new Server(server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST'],
+        },
+    })
     const channels = {}
     const sockets = {}
 
